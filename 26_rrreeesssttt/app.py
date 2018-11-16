d65fe9ff10323188749d3a6ba577d16bc31d5380
Tania Cao <tcaosu@stuy.edu>
2018-11-16 02:10:30 -0500

    add 26_rrreeesssttt

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -4,18 +4,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    breeds = "https://catfact.ninja/breeds"
-    facts = "https://catfact.ninja/fact"
-    bd = urllib.request.urlopen(breeds)
-    ft = urllib.request.urlopen(fact)
-		b = bd.read()
-		f = ft.read()
-    breed = json.loads(b)
-		fact = json.loads(f)
-    breed = breed["data"]
-		fact = fact["fact"]
-    return render_template("url.html", breed = breed, fact = fact)
+    # CAT FACTS ---------------------------------------------------------------------------
+    fact = json.loads(urllib.request.urlopen("https://catfact.ninja/fact").read())
+    fact = fact["fact"]
+    ubreed = "https://catfact.ninja/breeds?limit=1&page="
+    breeds = []
+    for num in range(5):
+        dbreed = json.loads(urllib.request.urlopen(ubreed + str(random.randint(1,99))).read())
+        breeds += dbreed["data"]
+
+    # RANDOM POEMS ------------------------------------------------------------------------
+    poems = json.loads(urllib.request.urlopen("https://www.poemist.com/api/v1/randompoems").read())
+    poem = poems[random.randint(0,5)]
+
+    # WIKIPEDIA ---------------------------------------------------------------------------
+    advice = json.loads(urllib.request.urlopen("https://api.adviceslip.com/advice").read())
+    advice = advice["slip"]
+    
+    return render_template("url.html", fact = fact, breeds = breeds, poem = poem, advice = advice)
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
+    
