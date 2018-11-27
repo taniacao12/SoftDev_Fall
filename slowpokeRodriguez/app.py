eae10122421e4577d899bdcc3d821da47da33478
taniacao12 <tcaosu@stuy.edu>
2018-11-27 13:31:28 -0500

    update repo

--- slowpokeRodriguez/app.py
+++ slowpokeRodriguez/app.py
@@ -1,6 +1,36 @@
-from flask import Flask, render_template, request,session,url_for,redirect,flash
-import sqlite3
-import os
-import csv
-import time
+import os, csv, time, sqlite3
 
+from flask import Flask, render_template, request,session,url_for,redirect,flash
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(32) #key for session
+
+user = "a"
+passw = "b"
+
+@app.route("/")
+def home():
+    if "logged_in" in session:
+        return redirect(url_for("home"))
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/auth")
+def auth():
+    if user == request.args["username"] and passw == request.args["password"]:
+        session["logged_in"] = request.form["user"]
+        return redirect(url_for("home"))
+
+    flash("Username or password is incorrect")
+    return render_template("login.html")
+
+
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
